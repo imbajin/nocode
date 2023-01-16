f734dda736fba6317a9aa2ca7aa4832ddc15b7ad
imbajin <jin@apache.org>
2023-01-17 03:26:53 +0800

    Update validate-release.sh

--- .github/workflows/scripts/validate-release.sh
+++ .github/workflows/scripts/validate-release.sh
@@ -41,12 +41,12 @@ gpg --version 1>/dev/null || exit
 wget https://downloads.apache.org/incubator/hugegraph/KEYS || exit
 gpg --import KEYS
 # TODO: how to trust all public keys in gpg list, currently only trust the first one
-echo -e "5\ny\n" | gpg --no-tty --command-fd 0 --edit-key $3 trust
+echo -e "5\ny\n" | gpg --batch --command-fd 0 --edit-key $3 trust
 
 echo "trust all pk"
-for key in $(gpg --no-tty --list-keys --with-colons | awk -F: '/^pub/ {print $5}'); do
-  echo -e "5\ny\n" | gpg --no-tty --command-fd 0 --edit-key "$key" trust
-done
+#for key in $(gpg --no-tty --list-keys --with-colons | awk -F: '/^pub/ {print $5}'); do
+#  echo -e "5\ny\n" | gpg --batch --command-fd 0 --edit-key "$key" trust
+#done
 
 # step3: check sha512 & gpg signature
 for i in *.tar.gz; do
